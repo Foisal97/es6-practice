cd8ce1bfb914ef1e34de36ed3bdbb378b0a55cbb
foisal97 <foisalreza40@gmail.com>
2020-03-04 18:19:35 +0600

    true-false

---
+++ advance.js
@@ -0,0 +1,13 @@
+//false
+//0 "" undefined null nan
+//truth 
+//"0" ''
+const name="shakib khan";
+if(name.length>0)
+{
+    console.log("condition is true");
+}
+else
+{
+    console.log("condition is false");
+}
