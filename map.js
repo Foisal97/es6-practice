562ccee7f140bb5d17a2f5c5e09ff94a85472238
foisal97 <foisalreza40@gmail.com>
2020-03-05 21:59:05 +0600

    scope

--- map.js
+++ map.js
@@ -10,3 +10,12 @@ console.log(id);
 console.log(names);
 const big=stu.filter(s=> s.id>30);
 console.log(big);
+
+
+function add(num1,num2)
+{
+    console.log([...arguments]);
+    return num1+num2+arguments[2];
+}
+const result=add(2,3,5,7);
+console.log(result);
